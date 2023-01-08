b0565884c105c99c4f8fdef47a32b4c471caa65e
Your Name <akalimol@e1r3p17.clusters.42paris.fr>
2023-01-08 17:37:18 +0100

    message

---
+++ arrayHashing/128_longestConsecutiveSequence.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+        nums.sort()
+        longest = 1
+        temp = 1
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                continue
+            if nums[i] == nums[i - 1] + 1:
+                temp += 1
+            else:
+                if temp > longest:
+                    longest = temp
+                temp = 1
+        if temp > longest:
+            longest = temp
+        return longest
