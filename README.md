5c31d067a86b484603e7e2ba60db82a5101b1d71
akadil.kalimoldayev@gmail.com <akadil.kalimoldayev@gmail.com>
2024-02-13 10:46:42 +0100

    Sorting - problem 23

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ My leetcode account [Link](https://leetcode.com/Akadil/)
 | 19 | [707 - Linked list design](https://leetcode.com/problems/design-linked-list/) | [Link - C++](https://github.com/Akadil/leetcode/blob/main/linkedList/707_linkedListDesign.cpp) | Linked list - medium | 78.80% | 6.6% |
 | 20 | [1472 - Design a browser history](https://leetcode.com/problems/design-browser-history/description/) | [Link - C++](https://github.com/Akadil/leetcode/blob/main/linkedList/1472_designBrowserHistory.cpp) | Linked list - medium | 63.97% | 21.24% |
 | 21 | [912 - Sort an array](https://leetcode.com/problems/sort-an-array/) | [Link - C++](https://github.com/Akadil/leetcode/blob/main/sorting/912_sortAnArray.cpp) | Sorting - medium | 38.95% | 26.54% |
-| 22 | [23 - merge K sorted arrays](https://leetcode.com/problems/merge-k-sorted-lists/) | [Link C++](https://github.com/Akadil/leetcode/blob/main/sorting/23_mergeKSortedList.cpp) | Sorting - hard | 5.10% | 45.59% |
+| 22 | [23 - merge K sorted arrays](https://leetcode.com/problems/merge-k-sorted-lists/) | [Link C++](https://github.com/Akadil/leetcode/blob/main/sorting/23_mergeKSortedList.cpp) | Sorting - hard | 49.83% | 44.80% |
 | |  |  |  |  |  |
 
 
--- sorting/23_mergeKSortedList.cpp
+++ sorting/23_mergeKSortedList.cpp
@@ -13,6 +13,83 @@ struct ListNode {
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
+        /*
+            Normally, I will merge lists one by one:
+
+                1 + 2, (1 + 2) + 3, (1 + 2 + 3) + 4, and etc
+            
+            But, it is not efficient, it more like an insertion sort, which
+            is not the optimal one, so we are going to use the merge sort 
+            algorithm
+
+            Merge ((1 + 8) + (2 + 7)) + ((3 + 6) + (4 + 5))
+        */
+
+        int n = lists.size();
+
+        if (n == 0)
+            return (nullptr);
+
+        // Traverse till only one sorted list left
+        while (n > 1) {
+
+            // Merge the first and last
+            for (int i = 0; i < n / 2; i++) {
+                lists[i] = mergeLists(lists[i], lists[n - i - 1]);
+            }
+
+            // Get the index of the last merged list
+            n = (n + 1) / 2;
+        }
+        return (lists[0]);
+    }
+
+
+    ListNode*   mergeLists(ListNode* left, ListNode* right) {
+        if (left == nullptr && right == nullptr)
+            return (nullptr);
+        else if (left == nullptr)
+            return (right);
+        else if (right == nullptr) 
+            return (left);
+        
+        ListNode    *head = nullptr;
+        ListNode    *tail = nullptr;
+
+        while (left != nullptr && right != nullptr) {
+            if (left->val < right->val) {
+                addNode(left, &head, &tail);
+                left = left->next;
+            } else {
+                addNode(right, &head, &tail);
+                right = right->next;
+            }
+        }
+
+        if (right != nullptr) {
+            addNode(right, &head, &tail);
+        } else if (left != nullptr) {
+            addNode(left, &head, &tail);
+        }
+        return (head);
+    }
+
+
+    void    addNode(ListNode *node, ListNode **head, ListNode **tail) {
+        if (*head == nullptr) {
+            *head = node;
+            *tail = node;
+        } else {
+            (*tail)->next = node;
+            *tail = node;
+        }
+    }
+};
+
+
+class SolutionOld {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
 
         ListNode    *head = nullptr;
         ListNode    *tail = nullptr;
