d56775f6e7a639cc2df2b50f3d3508fe413c281e
akadil.kalimoldayev@gmail.com <akadil.kalimoldayev@gmail.com>
2024-02-27 16:20:04 +0100

    Hashing - problem 705 easy

---
+++ hashing/705_designHashSet.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class MyHashSet {
+public:
+    MyHashSet() {
+        
+    }
+    
+    void add(int key) {
+        if (contains(key) == false)
+            hashSet.push_back(key);
+    }
+    
+    void remove(int key) {
+        vector<int>::iterator k = find(hashSet.begin(), hashSet.end(), key);
+        if (k != hashSet.end()) {
+            hashSet.erase(k);
+        }
+    }
+    
+    bool contains(int key) {
+        return (find(hashSet.begin(), hashSet.end(), key) != hashSet.end());
+    }
+
+private:
+    vector<int> hashSet;
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
