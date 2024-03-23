4de5a3da3d184136d71cd00422d8a141d97087bf
akadil.kalimoldayev@gmail.com <akadil.kalimoldayev@gmail.com>
2024-03-23 13:28:37 +0100

    Heaps, two heaps - problem 480 hard

--- heaps/480_slidingWindowMedian.cpp
+++ heaps/480_slidingWindowMedian.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <queue>
 #include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
@@ -18,8 +19,10 @@ public:
         // Inductive step, traverse through windows
         for (int i = 1; i < nums.size() - k + 1; ++i) {
             removeNum(nums[i - 1]);
+            removeToDelete();
             normalizeHeaps();
             addNum(nums[i + k - 1]);
+            removeToDelete();
             returner.push_back(findMedian());
         }
 
@@ -27,89 +30,74 @@ public:
     }
 
     double findMedian() {
-        if (lower.size() > higher.size())
+        if (lower_size > higher_size)
             return (lower.top());
-        else if (lower.size() < higher.size())
+        else if (lower_size < higher_size)
             return (higher.top());
         else
             return ((double)higher.top() / 2 + (double)lower.top() / 2);
     }
 
+    void    removeToDelete() {
+        while (lower.empty() == false && lower_toDelete[lower.top()] != 0) {
+            lower_toDelete[lower.top()] -= 1;
+            lower.pop();
+        }
+        while (higher.empty() == false && higher_toDelete[higher.top()] != 0) {
+            higher_toDelete[higher.top()] -= 1;
+            higher.pop();
+        }
+    }
 
     void    normalizeHeaps() {
-        if (lower.size() > higher.size() + 1) {
+        if (lower_size > higher_size + 1) {
             higher.push(lower.top());
+            higher_size++;
             higher_contains[lower.top()] += 1;
             lower_contains[lower.top()] -= 1;
             lower.pop();
+            lower_size--;
         }
-        else if (higher.size() > lower.size() + 1) {
+        else if (higher_size > lower_size + 1) {
             lower.push(higher.top());
+            lower_size++;
             lower_contains[higher.top()] += 1;
             higher_contains[higher.top()] -= 1;
             higher.pop();
+            higher_size--;
         }
     }
 
     void removeNum(int myNum) {
         int size = 0;
-        int num;
 
         if (higher.empty() == false && higher_contains[myNum] != 0) {
-            num = higher.top();
-            higher.pop();
+            higher_toDelete[myNum] += 1;
+            higher_size--;
+            higher_contains[myNum] -= 1;
 
-            while (num != myNum) {
-                lower.push(num);
-                size++;
-
-                if (higher.empty() == true)
-                    break;
-                num = higher.top();
-                higher.pop();
-            }
-            while (size != 0) {
-                higher.push(lower.top());
-                lower.pop();
-                size--;
-            }
-            if (num == myNum) {
-                higher_contains[num] -= 1;
-                return;
-            }
         }
-        if (lower.empty() == false) {
-            num = lower.top();
-            lower.pop();
-
-            while (num != myNum) {
-                higher.push(num);
-                size++;
-
-                num = lower.top();
-                lower.pop();
-            }
-            while (size != 0) {
-                lower.push(higher.top());
-                higher.pop();
-                size--;
-            }
-            lower_contains[num] -= 1;
+        else if (lower.empty() == false) {
+            lower_toDelete[myNum] += 1;
+            lower_size--;
+            lower_contains[myNum] -= 1;
         }
     }
 
     void addNum(int num) {
-        if (lower.size() == higher.size()) {
+        if (lower_size == higher_size) {
             if (higher.empty() == false && num > higher.top()) {
                 higher.push(num);
                 higher_contains[num] += 1;
+                higher_size++;
             }
             else { // num < lower.top() || (higher.top() > num > lower.top())
                 lower.push(num);
                 lower_contains[num] += 1;
+                lower_size++;
             }  
         }
-        else if (lower.size() > higher.size()) {
+        else if (lower_size > higher_size) {
             if (lower.top() < num) {
                 higher.push(num);
                 higher_contains[num] += 1;
@@ -120,8 +108,9 @@ public:
                 lower_contains[lower.top()] -= 1;
                 lower.pop();
                 lower.push(num);
-                lower_contains[num] += 1;
+                lower_contains[num] += 1;    
             }
+            higher_size++;
         }
         else {  // lower.size() < higher.size()
             if (higher.top() > num) {
@@ -136,14 +125,19 @@ public:
                 higher.push(num);
                 higher_contains[num] += 1;
             }
+            lower_size++;
         }
     }
 
 private:
     priority_queue<int>                             lower;  // maxHeap, 0
     priority_queue<int, vector<int>, greater<int>>  higher; // minHeap, 1
-    unordered_map<int, int>                              lower_contains;
-    unordered_map<int, int>                              higher_contains;
+    unordered_map<int, int>                         lower_contains;
+    unordered_map<int, int>                         higher_contains;
+    unordered_map<int, int>                         lower_toDelete;
+    unordered_map<int, int>                         higher_toDelete;
+    int                                             lower_size = 0;
+    int                                             higher_size = 0;
 };
 
 int main(void) {
@@ -153,16 +147,20 @@ int main(void) {
         [1,2,3,4,2,3,1,4,2]
         [7,9,3,8,0,2,4,8,3,9]
         [2147483647,1,2,3,4,5,6,7,2147483647]
+        [9,7,0,3,9,8,6,5,7,6]
+        [7,8,8,3,8,1,5,3,5,4]
     */
 
     // vector<int> nums = {1,3,-1,-3,5,3,6,7};
     // vector<int> nums2 = {1,2,3,4,2,3,1,4,2};
     // vector<int> nums3 = {7,9,3,8,0,2,4,8,3,9};
-    vector<int> nums4 = {2147483647,1,2,3,4,5,6,7,2147483647};
+    // vector<int> nums4 = {2147483647,1,2,3,4,5,6,7,2147483647};
+    // vector<int> nums5 = {9,7,0,3,9,8,6,5,7,6};
+    vector<int> nums6 = {7,8,8,3,8,1,5,3,5,4};
     int         k = 3;
 
     Solution    sol;
-    vector<double>  result = sol.medianSlidingWindow(nums4, 2);
+    vector<double>  result = sol.medianSlidingWindow(nums6, 3);
 
     for (auto& r : result)
         cout << r << " ";
