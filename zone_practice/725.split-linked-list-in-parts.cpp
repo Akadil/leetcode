16958c78816a530dfff2137421076efeb6d44479
Akadil Kalimoldayev <akadil.kalimoldayev@gmail.com>
2025-03-29 19:13:10 +0100

    [ADD] leetcode integration

---
+++ zone_practice/725.split-linked-list-in-parts.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=725 lang=cpp
+ *
+ * [725] Split Linked List in Parts
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        
+    }
+};
+// @lc code=end
