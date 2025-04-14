f6647331e68ee551e25a0e243f84e347d66bc57c
Akadil <akadil.kalimoldayev@gmail.com>
2025-04-14 18:13:47 +0200

    [ADD] 209 minimum size subarray sum

--- array-string/sliding-window/209-minimum-size-subarray-sum.cpp
+++ array-string/sliding-window/209-minimum-size-subarray-sum.cpp
@@ -6,18 +6,26 @@ using namespace std;
 
 class Solution {
 public:
+    /**
+     * @brief 209. Minimum Size Subarray Sum
+     * 
+     * Given an array of positive integers nums and a positive integer target, return the minimal length of a contiguous subarray 
+     * [numsl, numsl+1, ..., numsr-1, numsr] of which the sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+     */
     int minSubArrayLen(int target, vector<int>& nums) {
         int length = 2147483647;
         int sum = 0;
         int l = 0;
 
+        // Sliding window
         for (int r = 0; r < nums.size(); ++r) {
             sum += nums[r];
+
+            // Shrink the window until the window is valid
             while (sum >= target) {
                 if (sum >= target)
                     length = min(length, r - l + 1);
-                sum -= nums[l];
-                l++;
+                sum -= nums[l++];
             }
         }
         return (length == 2147483647) ? 0 : length;
