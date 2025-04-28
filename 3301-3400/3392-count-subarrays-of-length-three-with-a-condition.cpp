f67f624b89a64c44e94736ea04df22296bc8ff4a
Akadil <akadil.kalimoldayev@gmail.com>
2025-04-28 17:48:59 +0200

    [ADD] 3392

---
+++ 3301-3400/3392-count-subarrays-of-length-three-with-a-condition.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    public:
+        int countSubarrays(vector<int>& nums) {
+            // sliding window
+            int ans = 0;
+    
+            for (int i = 2; i < nums.size(); ++i) {
+                if (2 * (nums[i] + nums[i - 2]) == nums[i - 1]) {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    };
